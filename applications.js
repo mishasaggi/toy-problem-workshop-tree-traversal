c823ca82d1028b2d79b1944a4938d053d384a59a
mishasaggi <monisha.saggi@gmail.com>
2016-01-19 23:58:40 -0600

    Add lecture and study notes on applications.

---
+++ applications.js
@@ -0,0 +1,52 @@
+/*
+
+Some applications of DF:
+
+1. Detecting cycle in a graph 
+A graph has cycle if and only if we see a back edge during DFS. 
+So we can run DFS for the graph and check for back edges.
+
+2. Topological Sorting
+
+
+Some applications of BF:
+
+1. Peer to Peer Networks. In Peer to Peer Networks like BitTorrent, BFS is used to find all neighbor nodes.
+
+2. Crawlers in Search Engines: Crawlers build index using Bread First. 
+The idea is to start from source page and follow all links from source and 
+keep doing same. Depth First Traversal can also be used for crawlers, 
+but the advantage with Breadth First Traversal is that the depth or levels of 
+built tree can be limited.
+
+3. Social Networking Websites: In social networks, we can find people within a 
+given distance ‘k’ from a person using Breadth First Search till ‘k’ levels.
+
+4. GPS Navigation systems: BFS is used to find all neighboring locations.
+
+Many algorithms like Prim’s Minimum Spanning Tree and Dijkstra’s Single Source Shortest Path use structure similar to Breadth First Search.
+
+
+  Problem 1: Make a binary search tree
+
+  Problem 2: Duplicate a binary tree
+
+  Problem 3: Make a game tree to help select chess moves
+
+  Problem 4: Delete a binary tree (freeing all nodes)
+
+
+
+
+
+
+
+
+
+  Problem keys:
+  1 : in-order traversal
+  2 : pre-order traversal
+  3 : breadth first traversal
+  4 : post-order traversal
+
+*/
